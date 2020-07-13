bf74be1f225c0ac71319fe5f8efaf4e06d196cdb
piupaulgit <piupaul99design@gmail.com>
2020-07-14 01:30:19 +0530

    comment change

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/signout", (req, res) => {
-  res.send("signout running");
+  res.send("signing out");
 });
 
 module.exports = router;
