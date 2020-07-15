39829be1770921cd0297694dcb0ad50d1424fe87
piupaulgit <piupaul99design@gmail.com>
2020-07-16 01:20:16 +0530

    validation added

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const { check } = require("express-validator");
 const { signup, signout } = require("../controllers/auth");
 
-router.post("/signup", signup);
+// signup up route
+router.post(
+  "/signup",
+  [
+    check("name")
+      .isLength({ min: 3 })
+      .withMessage("name should be more that 3 char"),
+    check("email").isEmail().withMessage("Please provide valid email"),
+  ],
+  signup
+);
+
+// signout up route
 router.get("/signout", signout);
 
 module.exports = router;
