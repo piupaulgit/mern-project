12cdaf434a77c12b2f1574ffc61291b090ae076d
piupaulgit <piupaul99design@gmail.com>
2020-07-17 01:24:43 +0530

    sign in controller added

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -2,6 +2,7 @@ const User = require("../models/user");
 const { validationResult } = require("express-validator");
 const expressJwt = require("express-jwt");
 const jwt = require("jsonwebtoken");
+const user = require("../models/user");
 
 // signup controller
 exports.signup = (req, res) => {
@@ -25,7 +26,41 @@ exports.signup = (req, res) => {
 };
 
 // signin controller
-exports.signin = (req, res) => {};
+exports.signin = (req, res) => {
+  const { email, password } = req.body;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg,
+    });
+  }
+  User.findOne({ email }),
+    (req, res) => {
+      if (err) {
+        res.status(400).json({
+          error: "User email not found is database",
+        });
+      }
+
+      if (!User.authenticate(password)) {
+        return res.status(401).json({
+          error: "Email password does not match",
+        });
+      }
+
+      // create token
+      const token = jwt.sign({ _id: user._id }, process.env.SECRETKEY);
+      // put token in cookie
+      res.cookie("token", token);
+
+      // send response to frontend
+      const { _id, name, email, role } = user;
+      res.json({
+        token,
+        user: { _id, name, email, role },
+      });
+    };
+};
 // signout controller
 exports.signout = (req, res) => {
   res.send("I am signing out now");
