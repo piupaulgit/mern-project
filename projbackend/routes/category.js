1c55b7a890b61f45398b1be5f9a4393bd96d8d79
piupaulgit <piupaul99design@gmail.com>
2020-07-22 00:56:59 +0530

    category finished

--- projbackend/routes/category.js
+++ projbackend/routes/category.js
@@ -1,14 +1,44 @@
-const express = require('express');
-const { getUserById } = require('../controllers/user');
-const { getCategoryById, createCategory } = require('../controllers/category');
-const { isSignedIn, isAuthenticated, isAdmin } = require('../controllers/auth');
+const express = require("express");
+const { getUserById } = require("../controllers/user");
+const {
+  getCategoryById,
+  createCategory,
+  getCategory,
+  getAllCategory,
+  updateCategory,
+  removeCategory,
+} = require("../controllers/category");
+const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 const router = express.Router();
 
 module.exports = router;
 
 // params
-router.param("userId", getUserById)
-router.param("categoryId", getCategoryById)
+router.param("userId", getUserById);
+router.param("categoryId", getCategoryById);
 
 // actual routes
-router.post("/category/create/:userId", isSignedIn, isAuthenticated, isAdmin, createCategory)
+router.post(
+  "/category/create/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  createCategory
+);
+
+router.get("/category/:categoryId", getCategory);
+router.get("/categories", getAllCategory);
+router.put(
+  "/category/:categoryId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  updateCategory
+);
+router.delete(
+  "/category/:categoryId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  removeCategory
+);
