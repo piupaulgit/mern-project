7df62d205e9f6df47d5b0414b134e1104d5961d7
piupaulgit <piupaul99design@gmail.com>
2020-07-29 00:58:55 +0530

    link added

--- projfrontend/src/core/menu.js
+++ projfrontend/src/core/menu.js
@@ -1,27 +1,45 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
-
-const Menu = () => {
+const currentLink = (history, path) => {
+  if (history.location.pathname === path) {
+    return { color: "#fff" };
+  } else {
+    return { color: "#d2d1d1c2" };
+  }
+};
+const Menu = ({ history }) => {
   return (
     <nav className="navbar navbar-expand-sm bg-primary navbar-dark">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <Link to="/" className="nav-link">
+          <Link to="/" className="nav-link" style={currentLink(history, "/")}>
             Home
           </Link>
         </li>
         <li className="nav-item">
-          <Link to="/" className="nav-link">
+          <Link
+            to="/cart"
+            className="nav-link"
+            style={currentLink(history, "cart")}
+          >
             Cart
           </Link>
         </li>
         <li className="nav-item">
-          <Link to="/" className="nav-link">
+          <Link
+            to="/"
+            className="nav-link"
+            style={currentLink(history, "dashboard")}
+          >
             dashboard
           </Link>
         </li>
         <li className="nav-item">
-          <Link to="/" className="nav-link">
+          <Link
+            to="/"
+            className="nav-link"
+            style={currentLink(history, "adminDashboard")}
+          >
             Admin dashboard
           </Link>
         </li>
