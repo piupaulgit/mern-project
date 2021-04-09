2c35bd464b344ad9ede57c1ab505cf165d77777c
piupaulgit <contactpiu619@gmail.com>
2021-04-09 09:19:01 +0530

    order table added

--- projfrontend/src/admin/ManageOrders.js
+++ projfrontend/src/admin/ManageOrders.js
@@ -8,7 +8,7 @@ const ManageOrders = () => {
     const { user, token } = isAuthenticated();
     const getOrders = () => {
         getAllOrders(user._id, token).then(data => {
-            console.log(data,'oopo')
+            setOrders(data)
         })
     }
     useEffect(() => {
@@ -18,8 +18,47 @@ const ManageOrders = () => {
         <AdminBase title="Manage Orders">
               <div className="container-fluid p-5">
                 <div className="d-flex aling-items-center justify-content-between">
-                    <h2 className="heading mb-0">All Orders</h2>)
+                    <h2 className="heading mb-0">All Orders</h2>
                 </div>
+                {orders.length > 0 && 
+                    <div className="scrallable-div mt-4">
+                        <table className="table border">
+                            <thead className="bg-light">
+                                <tr >
+                                    <th className="font-weight-light">Serial Number</th>
+                                    <th className="font-weight-light">Order Id</th>
+                                    <th className="font-weight-light">Total Price</th>
+                                    <th className="font-weight-light">Status</th>
+                                    <th className="font-weight-light">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            {orders.map((odr,index) => {
+                                return (
+                                        <tr key={odr._id}>
+                                            <td>{index}</td>
+                                            <td>{odr._id}</td>
+                                            <td>{odr.amount}</td>
+                                            <td>{odr.status}</td>
+                                            <td>
+                                                <button className="btn btn-primary btn-sm mr-2">View Detail</button>
+                                                <button className="btn btn-primary btn-sm">Change Status</button>
+                                            </td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                            </tbody>
+                        </table>
+                    </div>
+                }
+                {
+                    orders.length === 0 && (
+                    <div className="alert alert-danger mt-3">
+                        No Orders Found in DB.
+                    </div>
+                    )
+                }
               </div>
         </AdminBase>
     )
