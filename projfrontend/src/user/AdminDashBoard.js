04958f56f927afb3f727b3dd2e9cea801db191e3
piupaulgit <contactpiu619@gmail.com>
2021-04-13 08:34:43 +0530

    dashboard

--- projfrontend/src/user/AdminDashBoard.js
+++ projfrontend/src/user/AdminDashBoard.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import { isAuthenticated } from '../auth/helper';
 import AdminBase from '../core/AdminBase'
 import Base from '../core/Base'
 
 const AdminDashboard = () => {
+    const { user, token } = isAuthenticated();
+    console.log(user)
     return (
         <AdminBase title="Admin dashboard">
             <div className="container-fluid p-5">
-                <div className="d-flex aling-items-center justify-content-between">
-                    <h2>dashboard</h2>
+                <div className="">
+                    <h2>Welcome {user.name}</h2>
+                    <p>Email: {user.email}</p>
                 </div>
             </div>
         </AdminBase>
